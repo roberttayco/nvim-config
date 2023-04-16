5d2b6be1ee2af7033964610f3a74837b9f456cc6
Robert Tayco <robert.tayco@gmail.com>
2023-04-16 17:51:10 -0500

    set up colorscheme, stop autoformat on save

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.expandtab = false
+opt.shiftwidth = 4
+opt.smarttab = true
+opt.softtabstop = 4
+opt.tabstop = 4
+
+opt.scrolloff = 8
+opt.colorcolumn = "80"
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+-- Set colorscheme
+return {
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
+}
