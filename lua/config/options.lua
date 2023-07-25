7af34ca0dfb278f25c62b683b5bc786fd2a0ad58
Robert Tayco <robert.tayco@nwea.org>
2023-07-25 09:54:18 -0500

    minor updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.vim_markdown_frontmatter = 1
+
 local opt = vim.opt
 
 opt.expandtab = false
 opt.shiftwidth = 4
 opt.smarttab = true
-opt.softtabstop = 4
 opt.tabstop = 4
+opt.softtabstop = 4
 
 opt.scrolloff = 8
 opt.colorcolumn = "80"
